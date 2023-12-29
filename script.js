38e9d37c2a6e5a859034807c107693f6429cd273
shivanisah <shivanisah66@gmail.com>
2023-12-29 18:29:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector("#btn")
+
+btn.addEventListener("click",() => {
+    console.log("if your answer is yes so click OK and if you answer is No so click cancel🥺 ")
+  
+   confirm("I am sorry babe ")
+
+//    console.log(remove.innerText)
+   confirm("please baby mann jao naa")
+
+   confirm("I am ur sweet bubu naaa")
+
+   alert("Thankyou baby mann gya😘")
+    console.log("Ab humko utna changes krne nhi ata yes or no toh smj aao aap ")
+
+})
